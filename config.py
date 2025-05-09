4cc6cbdc8c43533da5a26b9f0c2e52ddd6525c0d
AllSystem88 <deependray228@gmail.com>
2025-05-09 12:08:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 # Vars For API End Pont.
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-API_KEY = getenv("API_KEY", 30DxNexGenBotsc0db7b) # youtube song api key, get it from https://t.me/RahulTC
+API_KEY = getenv("API_KEY", "30DxNexGenBotsc0db7b") # youtube song api key, get it from https://t.me/RahulTC
 
 ## Other vaes
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 300))
