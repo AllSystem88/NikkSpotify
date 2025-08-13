6712ea1eca4eb850749843eb0fd32bcae85cee6c
DeepVikky88 <radheymusic51@gmail.com>
2025-08-13 15:38:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/OWNERSYSTUMM/Spotify",
+    "https://github.com/DeepVikky88/NikkSpotify",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
