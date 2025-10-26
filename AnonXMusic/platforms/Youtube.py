b7f5938e262b74bd2830b2acc380abf247ba4b20
AllSystem88 <deependray228@gmail.com>
2025-10-26 18:30:57 +0530

    Update Youtube.py

--- AnonXMusic/platforms/Youtube.py
+++ AnonXMusic/platforms/Youtube.py
@@ -26,9 +26,9 @@ import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
-from BADMUSIC import app
-from BADMUSIC.utils.database import is_on_off
-from BADMUSIC.utils.formatters import time_to_seconds
+from AnonXMusic import app
+from AnonXMusic.utils.database import is_on_off
+from AnonXMusic.utils.formatters import time_to_seconds
 
 
 _cached_cookie = None
